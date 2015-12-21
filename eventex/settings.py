ca2c87935781c13c8858266ccb84bb2ff1a3d378
Emerson Henning <emerson@henning.com.br>
2015-12-21 14:17:10 -0200

    typo correct

--- eventex/settings.py
+++ eventex/settings.py
@@ -80,9 +80,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': config('DATABASE_URL', default_dburl=default_dburl, cast=dburl),
-
-
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
