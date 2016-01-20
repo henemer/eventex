315cfa432ec76bf439566083cd5fa88ecdf8c6e8
Emerson Henning <emerson@henning.com.br>
2016-01-20 12:19:09 -0200

    REaliza inscricao

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,32 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form' :SubscriptionForm()}
+    if request.method == 'POST':
+        form  = SubscriptionForm(request.POST)
+        if form.is_valid():
 
-    return render(request, 'subscriptions/subscription_form.html', context )
+            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
+
+            mail.send_mail('Confirmação de inscrição',
+                           body,
+                           'emerson@henning.com.br',
+                           ['emerson@henning.com.br', form.cleaned_data['email']])
+
+            messages.success(request, 'Inscrição realizada com sucesso!')
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html',
+                          {'form': form})
+
+    else:
+        context = {'form' :SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context )
+
+
